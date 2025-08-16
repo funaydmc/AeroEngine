87ac3ecd65655c1e9f8bb7cc771625e8e7202b99
Funayd <105151651+funaydmc@users.noreply.github.com>
2025-08-16 13:31:28 +0700

    Remove CommandAPI integration and shadow plugin

--- src/main/java/com/github/funaydmc/aeroEngine/AeroEnginePlugin.java
+++ src/main/java/com/github/funaydmc/aeroEngine/AeroEnginePlugin.java
@@ -1,24 +1,18 @@
 package com.github.funaydmc.aeroEngine;
 
-import dev.jorel.commandapi.CommandAPI;
-import dev.jorel.commandapi.CommandAPIBukkitConfig;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AeroEnginePlugin extends JavaPlugin {
 
     @Override
     public void onLoad() {
-        CommandAPI.onLoad(new CommandAPIBukkitConfig(this));
     }
 
     @Override
     public void onEnable() {
-        CommandAPI.onEnable();
-
     }
 
     @Override
     public void onDisable() {
-        CommandAPI.onDisable();
     }
 }
